468479b141e5c59b5dcf0b6a31add50df9762a62
Gino Santonicola <luigisantonicola1988@gmail.com>
2025-01-14 00:43:58 +0100

    aggiunta Oggetti Animale e Gatto e fatto override della stampa del verso

---
+++ src/Animale.java
@@ -0,0 +1,12 @@
+//Scrivere un programma che contenga una classe chiamata Animale ed un metodo
+// chiamato faiIlVerso() che stampi il verso dell'animale.
+//Crea poi una sottoclasse chiamata Gatto che fa override del metodo
+//faiIlVerso() per miagolare. Prova quindi a far stampare il verso di Animale e di Gatto.
+
+public class Animale {
+
+    public void faiIlVerso(){
+        System.out.println("il verso del gatto è: ");
+    }
+
+}
---
+++ src/Gatto.java
@@ -0,0 +1,8 @@
+public class Gatto extends Animale{
+
+    @Override
+    public void faiIlVerso() {
+        super.faiIlVerso();
+        System.out.println(" Miao");
+    }
+}
